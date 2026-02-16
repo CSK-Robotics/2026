981f60b6010c5be812503a10ecf38ab2124828c5
Patrice White <patricewhite2012@gmail.com>
2026-02-16 18:50:37 -0500

    updated robot java. Added reset

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,9 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.event.EventLoop;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.subsystems.Shooter;
 import static frc.robot.Constants.FuelConstants.*;
@@ -16,52 +19,72 @@ public class Robot extends TimedRobot {
   private final XboxController m_controller = new XboxController(0);
   private final CommandXboxController m_controller2 = new CommandXboxController(1);
   private final Drivetrain m_swerve = new Drivetrain();
+  private final EventLoop m_loop = new EventLoop(); 
   private final Shooter ballSubsystem = new Shooter();
 
   // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
   private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
   private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
   private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
+  private Command m_autonomousCommand;
+
+  public boolean prevX = false;
+
+ @Override
+  public void autonomousInit() {
+    m_swerve.setupAutonomousConfigure();
+    m_autonomousCommand = getAutonomousCommand();
+  }
 
   @Override
   public void autonomousPeriodic() {
-    driveWithJoystick(false);
-    m_swerve.updateOdometry();
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
-  // @Override
-  // public void teleopInit(){
-  //   m_controller.a(m_loop).ifH
-  // }
+  @Override
+  public void teleopInit() {
+    m_controller.a(m_loop).ifHigh(m_swerve::reset);
+  }
 
   @Override
   public void teleopPeriodic() {
-    driveWithJoystick(true);
+    if(!m_controller.getXButton())driveWithJoystick(true);
+
+    if( m_controller.getXButton()){
+      m_swerve.lockWheels();
+    }
+
     intake();
+    m_loop.poll();
+  }
+
+  @Override
+  public void robotPeriodic() {
+    m_swerve.updateOdometry();
+    CommandScheduler.getInstance().run();
   }
 
   private void driveWithJoystick(boolean fieldRelative) {
     // Get the x speed. We are inverting this because Xbox controllers return
     // negative values when we push forward.
-    final var xSpeed =
-        -m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftY(), 0.02))
-            * Drivetrain.kMaxSpeed;
+    final var xSpeed = Math.pow(-m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftY(), 0.02)), 3)
+        * Drivetrain.kMaxSpeed;
 
     // Get the y speed or sideways/strafe speed. We are inverting this because
     // we want a positive value when we pull to the left. Xbox controllers
     // return positive values when you pull to the right by default.
-    final var ySpeed =
-        -m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftX(), 0.02))
-            * Drivetrain.kMaxSpeed;
+    final var ySpeed = Math.pow(-m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftX(), 0.02)), 3)
+        * Drivetrain.kMaxSpeed;
 
     // Get the rate of angular rotation. We are inverting this because we want a
     // positive value when we pull to the left (remember, CCW is positive in
     // mathematics). Xbox controllers return positive values when you pull to
     // the right by default.
-    final var rot =
-        -m_rotLimiter.calculate(MathUtil.applyDeadband(m_controller.getRightX(), 0.02))
-            * Drivetrain.kMaxAngularSpeed;
-
+    final var rot = Math.pow(-m_rotLimiter.calculate(MathUtil.applyDeadband(m_controller.getRightX(), 0.02)), 3)
+        * Drivetrain.kMaxAngularSpeed;
     m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative, getPeriod());
   }
 
@@ -79,5 +102,22 @@ public class Robot extends TimedRobot {
     // the intake
     m_controller2.a()
         .whileTrue(ballSubsystem.runEnd(() -> ballSubsystem.eject(), () -> ballSubsystem.stop()));
+  }
+
+  public Command getAutonomousCommand() {
+    /*
+     * try {
+     * // Load the path you want to follow using its name in the GUI
+     * PathPlannerPath path = PathPlannerPath.fromPathFile("TestPath1");
+     * // Create a path following command using AutoBuilder. This will also trigger
+     * // event markers.
+     * return AutoBuilder.followPath(path);
+     * } catch (Exception e) {
+     * DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
+     * return Commands.none();
+     * }
+     */
+
+    return m_swerve.run(() -> m_swerve.drive(0.0, 0.5, 0.0, false, getPeriod())).until(() -> isTeleop());
   }
 }
